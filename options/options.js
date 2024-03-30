bc1dabb90fced60b0ac75b75836869f87d3e36fe
Crowdbotics <team@crowdbotics.com>
2024-03-30 21:38:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snowy-wave-47329",
+  url: "https://snowy-wave-47329.botics.co",
+  api: "https://snowy-wave-47329.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
